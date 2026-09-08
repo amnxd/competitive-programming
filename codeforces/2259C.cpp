8e6909f559e0f687aa21500aa59da7645cf41093
amnxd <bhaskarshah4235@gmail.com>
2026-09-08 19:44:57 +0530

    fixed-structure

--- codeforces/2259C.cpp
+++ codeforces/2259C.cpp
@@ -31,7 +31,32 @@ void setup_io() {
 }
 
 void solve() {
-    
+    ll n; cin>>n;
+ 
+    vi v(n);
+    for(int &i : v) cin >> i;
+ 
+    int end = v.size()-1, front = 0;
+ 
+    vi v2;
+    for(int i=0; i<v.size(); i++){
+        v2.push_back( abs(v[i]) );
+        if( v[front] == v[end] && v[front] !=0 ){
+            for(int j=front+1; j<= end; j++){
+                if( j == end) v2.push_back( abs(v[i]) );
+                else v2.push_back(0);
+            }
+            break;
+        }
+        else{
+            if( front ==1 || front != end ) front++;
+            if( end == 0 ) end--;
+        }
+    }
+    for(auto &it: v2){
+        cout << it << " ";
+    }
+    cout<<"\n";
 }
 
 int main() {
